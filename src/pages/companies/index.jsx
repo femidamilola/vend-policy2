4abae27fea07f0d66b1c2932f45f61519d95fd47
SemzyTheMan <92394924+SemzyTheMan@users.noreply.github.com>
2023-05-10 15:41:13 +0100

    why

--- src/pages/companies/index.jsx
+++ src/pages/companies/index.jsx
@@ -4,6 +4,7 @@ import { Button } from "../../../components/Button/button";
 import { useRouter } from "next/router";
 const Company = () => {
   const router = useRouter();
+  console.log("");
   const Header = ({ img, location, price }) => (
     <div className="w-[100%] px-[25px] py-[40px] flex flex-col items-center">
       <Image
@@ -12,6 +13,7 @@ const Company = () => {
         src={`/assets/partner${img}.png`}
         alt=""
       ></Image>
+
       <p style={{ color: "rgba(55, 68, 83, 0.44)" }} className="text-[15px]">
         {location}
       </p>
